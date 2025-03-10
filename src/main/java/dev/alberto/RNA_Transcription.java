07ab4f179453bea8ad926a3fd958452ff9d837d2
Algama17 <albertogarciamallada@gmail.com>
2025-03-10 18:52:52 +0100

    Create Code

--- src/main/java/dev/alberto/RNA_Transcription.java
+++ src/main/java/dev/alberto/RNA_Transcription.java
@@ -1,5 +1,26 @@
 package dev.alberto;
 
 public class RNA_Transcription {
-
+    public String transcribe(String dna) {
+        StringBuilder rna = new StringBuilder();
+        for (char c : dna.toCharArray()) {
+            switch (c) {
+                case 'G':
+                    rna.append('C');
+                    break;
+                case 'C':
+                    rna.append('G');
+                    break;
+                case 'T':
+                    rna.append('A');
+                    break;
+                case 'A':
+                    rna.append('U');
+                    break;
+                default:
+                    throw new IllegalArgumentException("Nucleótido inválido: " + c);
+            }
+        }
+        return rna.toString();
+    }
 }
